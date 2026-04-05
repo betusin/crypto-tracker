f0f9480980228ebe2aa62395ca34708aff1c7c51
Alžbeta Hajná <ahajna@typesoft.io>
2026-04-05 21:41:53 +0200

    feat: add flutter_localizations support and configure English and Czech locales

--- lib/common/widget/app_root.dart
+++ lib/common/widget/app_root.dart
@@ -1,5 +1,9 @@
 import 'package:crypto_tracker/auth/widget/auth_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+const _enLocale = Locale('en', 'GB');
+const _csLocale = Locale('cs', 'CZ');
 
 class AppRoot extends StatelessWidget {
   const AppRoot({super.key});
@@ -10,6 +14,13 @@ class AppRoot extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Crypto Tracker',
       theme: ThemeData(colorSchemeSeed: Colors.amber, brightness: Brightness.dark),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [_enLocale, _csLocale],
+      locale: _csLocale,
       home: AuthWrapper(),
     );
   }
